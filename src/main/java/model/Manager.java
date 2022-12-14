9f760bff110444c216b2a40dd7cd1c22597654a3
Emanuel <cirabisiemanuel99@gmail.com>
2022-12-14 13:02:18 +0100

    Manager creation

--- src/main/java/model/Manager.java
+++ src/main/java/model/Manager.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Manager {
+    public Boolean adminPermissions;
+
+
+    public Manager(Boolean adminPermissions) {
+        this.adminPermissions = adminPermissions;
+    }
+
+    public IsAnAdmin() {
+        return adminPermissions;
+    }
+
 }
